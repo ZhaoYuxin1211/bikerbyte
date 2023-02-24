3e8e6105c384c90f48c8220c687c2d1c157e7175
ZhaoYuxin1211 <zhaoyuxin97@foxmail.com>
2023-02-24 17:44:10 +0000

    test connection to mysql

--- testSQL.py
+++ testSQL.py
@@ -2,5 +2,5 @@ import sqlalchemy
 import pandas as pd
 
 engine = sqlalchemy.create_engine('mysql://admin:bikerbyte@database-bike.cm2v37iz6jos.eu-west-1.rds.amazonaws.com'
-                                  ':3306/database-bike')
-sql_DF = pd.read_sql("SELECT CURRENT_TIMESTAMP;", con=engine, parse_dates=['activity_date'])
+                                  ':3306/dbtest01')
+sql_DF = pd.read_sql("select * from table_test01;", con=engine)
