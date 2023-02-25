aefddd93611f3f25af6fabc873b2c5b3f24b0320
ZhaoYuxin1211 <zhaoyuxin97@foxmail.com>
2023-02-25 00:51:59 +0000

    successfully connected to AWS RDS

--- testSQL.py
+++ testSQL.py
@@ -1,6 +1,8 @@
 import sqlalchemy
+import pymysql
 import pandas as pd
 
-engine = sqlalchemy.create_engine('mysql://admin:bikerbyte@database-bike.cm2v37iz6jos.eu-west-1.rds.amazonaws.com'
-                                  ':3306/dbTest01')
+engine = sqlalchemy.create_engine('mysql+pymysql://admin:bikerbyte@database-bike.cm2v37iz6jos.eu-west-1.rds.amazonaws'
+                                  '.com:3306/dbTest01')
 sql_DF = pd.read_sql("select * from table_test01;", con=engine)
+print(sql_DF)
