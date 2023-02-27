6db88e664d0c4990ce1338118c21072a2b1727ed
ZhaoYuxin1211 <zhaoyuxin97@foxmail.com>
2023-02-27 17:33:08 +0000

    successfully connected to AWS RDS

---
+++ apiTest.py
@@ -0,0 +1,24 @@
+import time
+import traceback
+
+import dbinfo
+import requests
+import json
+from pprint import pprint
+
+
+def main():
+    while True:
+        try:
+            r = requests.get(dbinfo.STATIONS_URI, params={"apiKey": dbinfo.JCKEY, "contract": dbinfo.NAME})
+
+            # json.loads(r.text)
+
+            pprint(json.loads(r.text))
+
+            time.sleep(30)
+        except:
+            print(traceback.format_exc())
+
+
+main()
