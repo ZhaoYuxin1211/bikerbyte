1c6ca8bca739956273ef22d4287efe053e48cec1
ZRQ-rikkie <Z986929216@gmail.com>
2023-02-28 10:53:58 +0000

    insert data to the availability table

--- DAO/table_insert.py
+++ DAO/table_insert.py
@@ -18,31 +18,43 @@ import time
 
 from IPython.display import display
 
-#do not assign the database at first.
+# do not assign the database at first.
 URI = "database-bike.cm2v37iz6jos.eu-west-1.rds.amazonaws.com"
 PORT = "3306"
 USER = "admin"
-PASSWORD ="bikerbyte"
+PASSWORD = "bikerbyte"
 DB_SCHEMA = "bikerbytedata"
 
-
-#using mysql + pymysql, not the same as the slides.
-engine =sqla.create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER,PASSWORD,URI,PORT,DB_SCHEMA), echo=True)
+# using mysql + pymysql, not the same as the slides.
+engine = sqla.create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB_SCHEMA), echo=True)
 
 
 # def write_to_file(text):
 #     with open("data/bike_{}".format(now).replace(" ", "_"), "w") as f:
 #         f.write(r.text)
 
-def stations_to_db(text, vals=None):
+def stations_to_db(text):
     stations = json.loads(text)
     print(type(stations), len(stations))
     # print(stations)
     for station in stations:
         print(station)
         vals = (
-        station.get('number'), station.get('address'), int(station.get('banking')), station.get('bike_stands'), station.get('name'), station.get('position').get('lat'),
-        station.get('position').get('lng'))
+            int(station.get('number')), station.get('last_update'), int(station.get('available_bikes')),
+            int(station.get('available_bike_stands')), station.get('status'))
+        engine.execute("insert into station values(%s,%s,%s,%s,%s)", vals)
+    return
+
+
+def availability_to_db(text):
+    availability = json.loads(text)
+    print(type(availability), len(availability))
+    for a in availability:
+        print(availability)
+        vals = (
+            int(a.get('number')), a.get('address'), int(a.get('banking')),
+            a.get('bike_stands'), a.get('name'), a.get('position').get('lat'),
+            a.get('position').get('lng'))
         engine.execute("insert into station values(%s,%s,%s,%s,%s,%s,%s)", vals)
     return
 
@@ -55,14 +67,12 @@ def main():
             print(r, now)
             # write_to_file(r.text)
             stations_to_db(r.text)
+            availability_to_db(r.text)
             time.sleep(5 * 60)
         except:
             print(traceback.format_exc())
             if engine is None:
                 return
 
+
 main()
-
-
-
-
