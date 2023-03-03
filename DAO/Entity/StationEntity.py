b2af7c88fafab6f52c8adfb7751a88d069df43ac
ZhaoYuxin1211 <zhaoyuxin97@foxmail.com>
2023-03-03 10:29:21 +0000

    get station data from database and store the stations in a list

--- DAO/Entity/StationEntity.py
+++ DAO/Entity/StationEntity.py
@@ -1,9 +1,22 @@
 class Station:
     def __init__(self, number, address, banking, bike_stands, name, position_lat, position_lng):
-        self.id = number
+        self.number = number
         self.address = address
         self.banking = banking
         self.bikeStands = bike_stands
         self.name = name
         self.positionLat = position_lat
-        self.positonLng = position_lng
+        self.positionLng = position_lng
+        self.lastUpdate = 0
+        self.availableBikes = 0
+        self.availableBikeStands = 0
+        self.status = 0
+
+    def getAvailability(self, last_update, availableBikes, availableBikeStands, status):
+        self.lastUpdate = last_update
+        self.availableBikes = availableBikes
+        self.availableBikeStands = availableBikeStands
+        self.status = status
+
+    def __str__(self) -> str:
+        return str(self.number) + ", " + self.name +","+ self.address +","+ str(self.availableBikes)+","+str(self.lastUpdate)
