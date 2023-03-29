52464442fcd9e523fba86a3d0e733d1f580fe1fc
leslieruan <leslieruan8@gmail.com>
2023-03-30 00:08:23 +0100

    add the available/<int:station_id> website

--- flasktest.py
+++ flasktest.py
@@ -27,13 +27,25 @@ def get_stations():
 
 @app.route("/available/<int:station_id>")
 def get_available_bikes(station_id):
-    # show the station with the given id, the id is an integer
-    available = json.loads(StationDAO())
+    # Get all station data
+    all_stations = StationDAO()
+
+    # Find the station with the given ID
+    station = next((s for s in all_stations if s['number'] == station_id), None)
+
+    if station is None:
+        # Station not found
+        return jsonify(error='Station not found'), 404
+
+    # Extract the available bikes data for the station
+    available_bikes = station['availableBikes']
+
+    return jsonify(available=available_bikes)
 
 if __name__ == "__main__":
 
     app.run(debug=True)
-print("======================================================================")
-print(get_stations())
+
+
 
 
