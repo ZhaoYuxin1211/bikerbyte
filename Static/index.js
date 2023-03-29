6a1bbb0ef8c0157fba3cec4376b47945de5545d5
ZRQ-rikkie <Z986929216@gmail.com>
2023-03-29 23:12:11 +0100

    keep change

--- Static/index.js
+++ Static/index.js
@@ -1,48 +1,49 @@
-function addMarkers(data){
-    console.log(data)
-    const stations = data.station;
-    // console.log(stations.station)
-    stations.forEach(station =>{
+function addMarkers(stations){
+    // console.log(stations)
+    console.log(stations.station)
+    for (const station of stations){
         var marker = new google.maps.Marker({
             position:{
-                lat:Number(station.positionLat),
-                lng:Number(station.positionLng),
+                lat:station.position_lat,
+                lng:station.position_lng,
             },
             map:map,
             title:station.name,
             station_number:station.number,
         });
-    });
+    }
 }
 
-// }
+// function getStations() {
+//     fetch("/stations")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log("fetch response", typeof data);
+//             addMarkers(data);
+// });
+
 function getStations() {
     fetch("/stations")
-        .then((response) => response.json())
+        .then(response => {response.json();})
         .then((data) => {
-            console.log("fetch response", data);
+            console.log("fetch response", typeof data);
             addMarkers(data);
-        });
+});
 }
 
-function initMap() {
-    const dublin = {lat: 53.35014, lng: -6.266155};
+//initialize and add the map
+function initMap(){
+    const dublin = { lat: 53.35014, lng: -6.266155 };
 // The map, centered at Dublin
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
-        center: dublin,
+    zoom: 14,
+    center: dublin,
     });
-
+// const marker = new google.maps.Marker({
+// position: dublin,
+// map: map,
+// });
     getStations();
 }
-
 var map = null;
 window.initMap = initMap;
-
-
-// fetch("http://127.0.0.1:5000/stations")
-// .then((response) => response.json())
-// .then((data) => console.log(data));
-
-
-
