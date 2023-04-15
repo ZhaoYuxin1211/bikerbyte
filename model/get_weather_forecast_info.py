62473b4447203cf3b9a0fe0cb79acac497a6eabe
ZhaoYuxin1211 <zhaoyuxin97@foxmail.com>
2023-04-15 23:26:28 +0100

    update get_weather_forecast_info.py , return weather forecast info to show

--- model/get_weather_forecast_info.py
+++ model/get_weather_forecast_info.py
@@ -35,3 +35,30 @@ def get_weather_forecast():
         return None
 
 
+def get_weather_forecast_to_show():
+    try:
+        # get json from weather api
+        r = requests.get(dbinfo.WEATHER_FORCAST, params={"q": dbinfo.WEATHER_CITY, "appid": dbinfo.WEATHER_KEY})
+        weather_forecast = json.loads(r.text)
+
+        weatherForecastInfo = []
+        for weather in weather_forecast.get('list'):
+            timestamp = weather.get('dt')
+            dt = datetime.datetime.fromtimestamp(timestamp)
+            hour = dt.hour
+            current_hour = datetime.datetime.now().hour
+            # api only provide hour: 1\4\7\10\13\16\19\22 weather report
+            if (hour == current_hour) or (hour - 1 == current_hour) or (hour + 1 == current_hour):
+                date = dt.date()
+                main = weather.get('weather')[0].get('main')
+                temp = weather.get('main').get('temp')
+                weatherForecastInfo.append([date, main, temp])
+
+        return weatherForecastInfo
+
+    except:
+        print(traceback.format_exc())
+        return None
+
+
+get_weather_forecast_to_show()
